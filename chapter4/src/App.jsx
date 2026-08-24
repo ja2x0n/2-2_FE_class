8d15b20642fd996d4108c4db48847e4ff7eb3376
jae1 <t01046993182@gmail.com>
2026-08-24 15:03:06 +0900

    8/24 react study

--- chapter4/src/App.jsx
+++ chapter4/src/App.jsx
@@ -2,9 +2,6 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 
-const Child = () => {
-    return <p>Child Component</p>;
-};
 
 function App() {
     const title = "Hello";
